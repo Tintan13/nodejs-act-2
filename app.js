e47e26f3cb3036880cd45e045a264676c7690fe2
Tintan13 <lyndsayannecleofe6@gmail.com>
2024-09-06 14:46:45 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
